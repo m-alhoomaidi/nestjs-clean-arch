f4322a0839ec0c59c2d78d9b09c3bba891ac4e56
Mohammed Abdo <m.alhoomaidi@gmail.com>
2025-01-04 22:38:05 +0300

    add message to exception

--- src/core/category/exceptions/category.exception.ts
+++ src/core/category/exceptions/category.exception.ts
@@ -1,20 +1,27 @@
-export class CategoryNotFoundException extends Error {
+export class CategoryDomainException extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'CategoryDomainException';
+  }
+}
+
+export class InvalidCategoryDataException extends CategoryDomainException {
+  constructor(message: string) {
+    super(message);
+    this.name = 'InvalidCategoryDataException';
+  }
+}
+
+export class CategoryNotFoundException extends CategoryDomainException {
   constructor(id: number) {
     super(`Category with id ${id} not found`);
     this.name = 'CategoryNotFoundException';
   }
 }
 
-export class CategoryAlreadyExistsException extends Error {
+export class CategoryAlreadyExistsException extends CategoryDomainException {
   constructor(nameEn: string) {
     super(`Category with English name ${nameEn} already exists`);
     this.name = 'CategoryAlreadyExistsException';
-  }
-}
-
-export class InvalidCategoryDataException extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'InvalidCategoryDataException';
   }
 }
